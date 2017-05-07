30332377f0dce0da7e80567dc94a4a4a8f85bb92
Young-K. Suh <yksuh@yksuh-mbpro.local>
2017-05-07 18:31:09 +0900

    discontinuous operator studies

--- subopt-tods16/queries/new_discontinuous_op.sql
+++ subopt-tods16/queries/new_discontinuous_op.sql
@@ -12,8 +12,15 @@ CREATE TABLE DiscontOpNew_Step0 AS
 	select *
 	from DiscontOp_S0
 	where 
-	   runid = 252 
-	and querynum = 7;
+	   -- runid = 252 
+	--and querynum = 7
+  	--   runid = 269 
+	--and querynum = 8
+	--   runid = 418
+	--and querynum = 7
+  	 runid = 285
+	and querynum = 7
+	;
 ALTER TABLE DiscontOpNew_Step0 ADD PRIMARY KEY (runid,querynum,card,PLANID,opID,statName);
 -- queryid = 5338, runid = 252, querynumber = 7.
 --select * from DiscontOp_S0 where opname = 'ALL:Full Table Scan'
@@ -27,11 +34,16 @@ CREATE TABLE DiscontOpNew_Step1 AS
 	       t0.card, 
 	       t0.planid
 	from DiscontOp_S0 t0 
-	where runid = 252 
+	where 
+	--     runid = 252 
+	--and querynum = 7
+	--   runid = 418
+	--and querynum = 7
+ 	runid = 285
 	and querynum = 7
 	order by runid, querynum;
 ALTER TABLE DiscontOpNew_Step1 ADD PRIMARY KEY (runid,querynum,card);
-
+-- select * from DiscontOpNew_Step1 where card <= 100000 order by card asc
 DROP TABLE DiscontOpNew_Step2;
 CREATE TABLE DiscontOpNew_Step2 AS 
 	select  t0.dbms,
@@ -48,6 +60,7 @@ CREATE TABLE DiscontOpNew_Step2 AS
 	and t0.card < t1.card
 	order by runid, querynum, low_card asc; 
 ALTER TABLE DiscontOpNew_Step2 ADD PRIMARY KEY (runid,querynum,low_card,high_card);
+-- select * from DiscontOpNew_Ste
 
 DROP TABLE DiscontOpNew_Step3;
 CREATE TABLE DiscontOpNew_Step3 AS 
@@ -80,12 +93,13 @@ CREATE TABLE DiscontOpNew_Step4 AS
 	FROM
 		(SELECT distinct dbms, runid, querynum, low_card, max(high_card) as high_card
 		FROM DiscontOpNew_Step3
-		where high_card - low_card > 60000 and numPlans = 1 	
+		where high_card - low_card >= 60000 and numPlans = 1 	
 		group by dbms, runid, querynum, low_card) 
 	group by dbms, runid, querynum, high_card
 	order by runid, querynum, high_card desc;
 ALTER TABLE DiscontOpNew_Step4 ADD PRIMARY KEY (runid,querynum,low_card,high_card);
 --- select low_card, high_card from DiscontOpNew_Step4
+
 
 DROP TABLE DiscontOpNew_Step5;
 CREATE TABLE DiscontOpNew_Step5 AS 
@@ -133,3 +147,55 @@ ALTER TABLE DiscontOpNew_Step6 ADD PRIMARY KEY (runid,querynum,low_card,planid,o
 select dbms, runid, querynum, low_card, high_card, curr_card, planid, opname, oporder, cost from discontopnew_step1
 order by dbms, runid, querynum, low_card asc, oporder asc
 
+-- generate sequences of at least five cardinalities 
+DROP TABLE DiscontOpNew_Step7;
+CREATE TABLE DiscontOpNew_Step7 AS 
+	with atomic_seq
+	(SELECT t1.runid,
+	       t1.querynum,
+	       t1.low_card,
+	       t0.card as high_card
+	FROM DiscontOpNew_Step1 t0, DiscontOpNew_Step4 t1
+	WHERE t0.runid = t1.runid 
+	and t0.querynum = t1.querynum
+	and t1.low_card+40000 <= t0.card and t0.card+10000 <= t1.high_card-40000)
+	SELECT
+	FROM  atomic_seq t0,
+	      DiscontOpNew_Step1 t1
+	WHERE 
+	    t0.runid 	= t1.runid
+	and t0.querynum = t1.querynum
+	and t0.low_card+40000 <= t1.card and t1.card+10000 <= t0.high_card-40000)
+ALTER TABLE DiscontOpNew_Step7 ADD PRIMARY KEY (runid,querynum,low_card,high_card);
+
+-- 2 operators
+WITH operatorCard AS 
+(SELECT dbms, runid, querynum, card, COUNT(*) AS noOperators
+FROM NSOOper_S1_QatC_Op
+GROUP BY dbms, runid, querynum, card
+HAVING COUNT(*)=2)
+SELECT o1.dbms, a1.opname||', '||a2.opname AS operatorName, o1.noOperators, COUNT(*) as numQatCs
+FROM operatorCard o1, NSOOper_S1_QatC_Op a1, NSOOper_S1_QatC_Op a2
+WHERE a1.dbms = o1.dbms
+AND a1.runid = o1.runid
+AND a1.querynum = o1.querynum
+AND a1.card = o1.card 
+AND a1.dbms = a2.dbms
+AND a1.runid = a2.runid
+AND a1.querynum = a2.querynum
+AND a1.card = a2.card
+AND a1.opname>a2.opname 
+GROUP BY o1.dbms, a1.opname||', '||a2.opname, o1.noOperators
+order by o1.dbms, numQatCs desc;
+
+
+
+  LOW_CARD  HIGH_CARD
+---------- ----------
+    100000     310000 100000, 
+    320000     550000
+    560000     990000
+   1000000    1650000
+   1660000    1990000
+
+--select low_card, high_card from DiscontOpNew_Step4
