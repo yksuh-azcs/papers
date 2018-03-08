f0801ee2dc2e9dd780853b87cac81d9b7b21eb37
Young-Kyoon Suh <yksuh@cs.arizona.edu>
2018-03-08 17:37:44 +0900

    03/08 draft

--- pt_characterization/real_data/hist.r
+++ pt_characterization/real_data/hist.r
@@ -56,6 +56,151 @@ xlab='PT (ms)', ylab=expression('Frequency'))
 axis(side=1,at=h$mids,labels=seq(xmin,xmax,1))
 dev.off()
 
+### SORT100
+numSamples <- 300
+x = read.csv(file="SORT100.dat",head=TRUE,sep="\t")
+x <- subset(x, x$ITERNUM <= numSamples)
+setEPS()
+postscript("sort100_dist.eps")
+binsize=1
+#x <- subset(x, x$ITERNUM != 9394)
+x_up = mean(x$PRTIME) + 2*sd(x$PRTIME)
+x_dn = mean(x$PRTIME) - 2*sd(x$PRTIME)
+x = subset(x, x$PRTIME >= x_dn & x$PRTIME <= x_up)
+xmin <-min(x$PRTIME)
+xmax <-max(x$PRTIME)
+xmin
+xmax
+nbins <- ceiling((xmax-xmin) / binsize)
+h = hist(x$PRTIME, right=F, breaks=seq(xmin,xmax+binsize,binsize),plot=F)
+xmin <-min(x$PRTIME)
+xmax <-max(x$PRTIME)
+ymax <- max(h$counts)
+ymax <- ceiling(ymax/100)*100
+ymax <- 150
+plot(h, xaxt='n', freq=TRUE,ylim=c(0,ymax), col="blue", main='PT frequency on SORT100 (sample size: 300)', 
+sub=paste("(n=",nrow(x),", bin_size=",binsize,"ms)",sep=""), 
+xlab='PT (ms)', ylab=expression('Frequency'))
+#axis(side=1,at=seq(min(h$mids),max(h$mids)+binsize,binsize),labels=seq(ceiling(min(h$mids)),ceiling(max(h$mids))+binsize,binsize))
+axis(side=1,at=h$mids,labels=seq(xmin,xmax,1))
+dev.off()
+
+### SORT200
+numSamples <- 300
+x = read.csv(file="SORT200.dat",head=TRUE,sep="\t")
+x <- subset(x, x$ITERNUM <= numSamples)
+setEPS()
+postscript("sort200_dist.eps")
+binsize=1
+#x <- subset(x, x$ITERNUM != 9394)
+x_up = mean(x$PRTIME) + 2*sd(x$PRTIME)
+x_dn = mean(x$PRTIME) - 2*sd(x$PRTIME)
+x = subset(x, x$PRTIME >= x_dn & x$PRTIME <= x_up)
+xmin <-min(x$PRTIME)
+xmax <-max(x$PRTIME)
+xmin
+xmax
+nbins <- ceiling((xmax-xmin) / binsize)
+h = hist(x$PRTIME, right=F, breaks=seq(xmin,xmax+binsize,binsize),plot=F)
+xmin <-min(x$PRTIME)
+xmax <-max(x$PRTIME)
+ymax <- max(h$counts)
+ymax <- ceiling(ymax/100)*100
+ymax <- 100
+plot(h, xaxt='n', freq=TRUE,ylim=c(0,ymax), col="blue", main='PT frequency on SORT200 (sample size: 300)', 
+sub=paste("(n=",nrow(x),", bin_size=",binsize,"ms)",sep=""), 
+xlab='PT (ms)', ylab=expression('Frequency'))
+#axis(side=1,at=seq(min(h$mids),max(h$mids)+binsize,binsize),labels=seq(ceiling(min(h$mids)),ceiling(max(h$mids))+binsize,binsize))
+axis(side=1,at=h$mids,labels=seq(xmin,xmax,1))
+dev.off()
+
+### SORT400
+numSamples <- 300
+x = read.csv(file="SORT400.dat",head=TRUE,sep="\t")
+x <- subset(x, x$ITERNUM <= numSamples)
+setEPS()
+postscript("sort400_dist.eps")
+binsize=1
+#x <- subset(x, x$ITERNUM != 9394)
+x_up = mean(x$PRTIME) + 2*sd(x$PRTIME)
+x_dn = mean(x$PRTIME) - 2*sd(x$PRTIME)
+x = subset(x, x$PRTIME >= x_dn & x$PRTIME <= x_up)
+xmin <-min(x$PRTIME)
+xmax <-max(x$PRTIME)
+xmin
+xmax
+nbins <- ceiling((xmax-xmin) / binsize)
+h = hist(x$PRTIME, right=F, breaks=seq(xmin,xmax+binsize,binsize),plot=F)
+xmin <-min(x$PRTIME)
+xmax <-max(x$PRTIME)
+ymax <- max(h$counts)
+ymax <- ceiling(ymax/100)*100
+ymax <- 60
+plot(h, xaxt='n', freq=TRUE,ylim=c(0,ymax), col="blue", main='PT frequency on SORT400 (sample size: 300)', 
+sub=paste("(n=",nrow(x),", bin_size=",binsize,"ms)",sep=""), 
+xlab='PT (ms)', ylab=expression('Frequency'))
+#axis(side=1,at=seq(min(h$mids),max(h$mids)+binsize,binsize),labels=seq(ceiling(min(h$mids)),ceiling(max(h$mids))+binsize,binsize))
+axis(side=1,at=h$mids,labels=seq(xmin,xmax,1))
+dev.off()
+
+### SORT800
+numSamples <- 300
+x = read.csv(file="SORT800.dat",head=TRUE,sep="\t")
+x <- subset(x, x$ITERNUM <= numSamples)
+setEPS()
+postscript("sort800_dist.eps")
+binsize=2
+#x <- subset(x, x$ITERNUM != 9394)
+x_up = mean(x$PRTIME) + 2*sd(x$PRTIME)
+x_dn = mean(x$PRTIME) - 2*sd(x$PRTIME)
+x = subset(x, x$PRTIME >= x_dn & x$PRTIME <= x_up)
+xmin <-min(x$PRTIME)
+xmax <-max(x$PRTIME)
+xmin
+xmax
+nbins <- ceiling((xmax-xmin) / binsize)
+h = hist(x$PRTIME, right=F, breaks=seq(xmin,xmax+binsize,binsize),plot=F)
+xmin <-min(x$PRTIME)
+xmax <-max(x$PRTIME)
+ymax <- max(h$counts)
+ymax <- ceiling(ymax/100)*100
+ymax <- 60
+plot(h, xaxt='n', freq=TRUE,ylim=c(0,ymax), col="blue", main='PT frequency on SORT800 (sample size: 300)', 
+sub=paste("(n=",nrow(x),", bin_size=",binsize,"ms)",sep=""), 
+xlab='PT (ms)', ylab=expression('Frequency'))
+#axis(side=1,at=seq(min(h$mids),max(h$mids)+binsize,binsize),labels=seq(ceiling(min(h$mids)),ceiling(max(h$mids))+binsize,binsize))
+axis(side=1,at=h$mids,labels=seq(xmin,xmax,binsize))
+dev.off()
+
+### SORT1000
+numSamples <- 300
+x = read.csv(file="SORT1000.dat",head=TRUE,sep="\t")
+x <- subset(x, x$ITERNUM <= numSamples)
+setEPS()
+postscript("sort1000_dist.eps")
+binsize=1
+#x <- subset(x, x$ITERNUM != 9394)
+x_up = mean(x$PRTIME) + 2*sd(x$PRTIME)
+x_dn = mean(x$PRTIME) - 2*sd(x$PRTIME)
+x = subset(x, x$PRTIME >= x_dn & x$PRTIME <= x_up)
+xmin <-min(x$PRTIME)
+xmax <-max(x$PRTIME)
+xmin
+xmax
+nbins <- ceiling((xmax-xmin) / binsize)
+h = hist(x$PRTIME, right=F, breaks=seq(xmin,xmax+binsize,binsize),plot=F)
+xmin <-min(x$PRTIME)
+xmax <-max(x$PRTIME)
+ymax <- max(h$counts)
+ymax <- ceiling(ymax/100)*100
+ymax <- 20
+plot(h, xaxt='n', freq=TRUE,ylim=c(0,ymax), col="blue", main='PT frequency on SORT1000 (sample size: 300)', 
+sub=paste("(n=",nrow(x),", bin_size=",binsize,"ms)",sep=""), 
+xlab='PT (ms)', ylab=expression('Frequency'))
+#axis(side=1,at=seq(min(h$mids),max(h$mids)+binsize,binsize),labels=seq(ceiling(min(h$mids)),ceiling(max(h$mids))+binsize,binsize))
+axis(side=1,at=h$mids,labels=seq(xmin,xmax,binsize))
+dev.off()
+
 ### SORT1160
 numSamples <- 40
 x = read.csv(file="SORT1160.dat",head=TRUE,sep="\t")
@@ -79,6 +224,35 @@ ymax <- max(h$counts)
 ymax <- ceiling(ymax/100)*100
 ymax <- 10
 plot(h, xaxt='n', freq=TRUE,ylim=c(0,ymax), col="blue", main='PT frequency on SORT1160 with 30 samples', 
+sub=paste("(n=",nrow(x),", bin_size=",binsize,"ms)",sep=""), 
+xlab='PT (ms)', ylab=expression('Frequency'))
+#axis(side=1,at=seq(min(h$mids),max(h$mids)+binsize,binsize),labels=seq(ceiling(min(h$mids)),ceiling(max(h$mids))+binsize,binsize))
+axis(side=1,at=h$mids,labels=seq(xmin,xmax,binsize))
+dev.off()
+
+### SORT1160
+numSamples <- 300
+x = read.csv(file="SORT1160.dat",head=TRUE,sep="\t")
+x <- subset(x, x$ITERNUM <= numSamples)
+setEPS()
+postscript("sort1160_dist.eps")
+binsize=1
+#x <- subset(x, x$ITERNUM != 9394)
+x_up = mean(x$PRTIME) + 2*sd(x$PRTIME)
+x_dn = mean(x$PRTIME) - 2*sd(x$PRTIME)
+x = subset(x, x$PRTIME >= x_dn & x$PRTIME <= x_up)
+xmin <-min(x$PRTIME)
+xmax <-max(x$PRTIME)
+xmin
+xmax
+nbins <- ceiling((xmax-xmin) / binsize)
+h = hist(x$PRTIME, right=F, breaks=seq(xmin,xmax+binsize,binsize),plot=F)
+xmin <-min(x$PRTIME)
+xmax <-max(x$PRTIME)
+ymax <- max(h$counts)
+ymax <- ceiling(ymax/100)*100
+ymax <- 30
+plot(h, xaxt='n', freq=TRUE,ylim=c(0,ymax), col="blue", main='PT frequency on SORT1160 (sample size: 300)', 
 sub=paste("(n=",nrow(x),", bin_size=",binsize,"ms)",sep=""), 
 xlab='PT (ms)', ylab=expression('Frequency'))
 #axis(side=1,at=seq(min(h$mids),max(h$mids)+binsize,binsize),labels=seq(ceiling(min(h$mids)),ceiling(max(h$mids))+binsize,binsize))
