4c50a3ce22675be0678b9b28b3d30b6dfdbfaaec
Young-K. Suh <yksuh@yksuh-mbpro.local>
2018-04-26 23:46:17 +0900

    Next version

---
+++ pt_characterization/real_data/daemon_invest.sql
@@ -0,0 +1,29 @@
+-- Writer: Young-Kyoon Suh (yksuh@cs.arizona.edu)
+-- Date: 01/11/17
+-- Description: Define tables/views for EMP evaluation on INC programs 
+
+DROP TABLE DIFF_INC_RUN_PROC_INFO CASCADE CONSTRAINTS;
+CREATE TABLE DIFF_INC_RUN_PROC_INFO AS
+	SELECT arr.algrunid,
+	       ar.exp_run_time,
+	       arr.iternum,
+	       arr.runtime,
+	       proc.procid,
+	       proc.processname,
+	       (proc.utime+proc.stime)/1000 as pt
+	FROM AZDBLab_NewAlgRun2 ar, AZDBLab_NewAlgRunResult2 arr, AZDBLab_ProcInfo2 proc
+	WHERE 
+	    ar.algrunid = arr.algrunid
+	and arr.algrunid = proc.algrunid
+	and arr.datanum = proc.DATANUM
+	and arr.iternum = proc.iternum 
+	and (proc.utime+proc.stime) > 0
+	and arr.algrunid IN (
+				23921,23941,23961,23981,
+				24001,24021,24041,24061,
+				24081,24101,24121,24141
+			    )
+	order by iternum, pt desc;
+ALTER TABLE DIFF_INC_RUN_PROC_INFO ADD PRIMARY KEY (algrunid, iternum, procid,processname);
+
+
