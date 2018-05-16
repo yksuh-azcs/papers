4f3a0b9929e90ea08014cfcf02d13d795b53587c
Young-K. Suh <yksuh@yksuh-mbpro.local>
2018-05-16 23:25:10 +0900

    Added INC13

--- pt_characterization/machine_cons_check/new_INC_data_s9/inc_hist.r
+++ pt_characterization/machine_cons_check/new_INC_data_s9/inc_hist.r
@@ -1,3 +1,33 @@
+### sodb8-INC13
+numSamples <- 1000
+x = read.csv(file="INC13.0.dat",head=TRUE,sep="\t")
+x <- subset(x, x$ITERNUM <= numSamples)
+setEPS()
+postscript("s8_INC13_dist.eps")
+binsize=1
+#x <- subset(x, x$ITERNUM != 9394)
+x_up = mean(x$PRTIME) + 2*sd(x$PRTIME)
+x_dn = mean(x$PRTIME) - 2*sd(x$PRTIME)
+x = subset(x, x$PRTIME >= x_dn & x$PRTIME <= x_up)
+xmin <-min(x$PRTIME)
+xmax <-max(x$PRTIME)
+xmin
+xmax
+nbins <- ceiling((xmax-xmin) / binsize)
+h = hist(x$PRTIME, right=F, breaks=seq(xmin,xmax+binsize,binsize),plot=F)
+xmin <-min(x$PRTIME)
+xmax <-max(x$PRTIME)
+ymax <- max(h$counts)
+ymax <- ceiling(ymax/100)*100
+ymax <- 400 
+title <- paste("PT frequency of INC13 on sodb8 (sample size: ",numSamples,")")
+plot(h, xaxt='n', freq=TRUE,ylim=c(0,ymax), col="blue", main=title, 
+sub=paste("(n=",nrow(x),", bin_size=",binsize,"ms)",sep=""), 
+xlab='PT (ms)', ylab=expression('Frequency'))
+#axis(side=1,at=seq(min(h$mids),max(h$mids)+binsize,binsize),labels=seq(ceiling(min(h$mids)),ceiling(max(h$mids))+binsize,binsize))
+axis(side=1,at=h$mids,labels=seq(xmin,xmax,1))
+dev.off()
+
 ### sodb9-INC1
 numSamples <- 1000
 x = read.csv(file="INC1.0.dat",head=TRUE,sep="\t")
